e40649e8a547896c17556a7be17075bfac1241f3
EltsVV <evv_86@mail.ru>
2025-11-25 18:57:03 +0300

    Fix UI

--- Chat.Core/Interfaces/IUIService.cs
+++ Chat.Core/Interfaces/IUIService.cs
@@ -5,8 +5,8 @@ namespace Chat.Core.Interfaces;
 public interface IUIService
 {
     void DisplayMessage(ChatMessage message);
+    void DisplayMessage(string recipient, string message);
     void DisplaySystemMessage(string message);
     void DisplayErrorMessage(string message);
     void DisplayUserList(IEnumerable<User> users);
-    void DisplayP2PMessageConfirmation(string recipient, string message);
 }
